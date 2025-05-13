98560f506b8498c1dfba20c8f11d26b7622a025c
Toni Daada <tonidaada@gmail.com>
2025-05-13 15:32:42 +0100

    modified:   .gitignore 	new file:   public/pizzas/focaccia.jpg 	new file:   public/pizzas/funghi.jpg 	new file:   public/pizzas/margherita.jpg 	new file:   public/pizzas/prosciutto.jpg 	new file:   public/pizzas/salamino.jpg 	new file:   public/pizzas/spinaci.jpg 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = function () {
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>;
+    </header>
+  );
+};
+const Menu = function () {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      {pizzaData.map((data, id) => (
+        <Pizza
+          key={id}
+          name={data.name}
+          ingredients={data.ingredients}
+          photoName={data.photoName}
+          price={data.price}
+        />
+      ))}
+    </div>
+  );
+};
+
+function Pizza(props) {
+  // console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt="Pizza Spinaci" />
+      <div>
+        <h3> {props.name} </h3>
+        <p>{props.ingredients}</p>
+        <span>${props.price}</span>
+      </div>
+    </div>
+  );
+}
+const Footer = function () {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(`${isOpen}`);
+
+  // hour <= openHour && hour >= closeHour
+  //   ? alert("We are  closed")
+  //   : alert(`We are open`);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}
+      {isOpen ? ` We're currently open!` : ` We're currently closed`}
+    </footer>
+  );
+
+  // React.createElement("footer", null, `We're currenttly open!`);
+};
+
+// How we render the root in React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
